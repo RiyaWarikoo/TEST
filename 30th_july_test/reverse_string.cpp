233b5d18d733dd53a09cfc3aff3ec4f236109cad
Riya Warikoo <121093317+RiyaWarikoo@users.noreply.github.com>
2024-07-30 13:53:27 +0530

    Add files via upload

---
+++ 30th_july_test/reverse_string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverseString(string& str) {
+    int left = 0;
+    int right = str.size() - 1;
+    while (left < right) {
+        swap(str[left], str[right]);
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    string str;
+    cin >> str;
+    reverseString(str);
+    cout << str << endl;
+    return 0;
+}
